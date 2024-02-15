3302a42a386725ec949e93c2a0dc8a8ea9a62b19
vaibhavPatil222 <ptanmay>
2024-02-15 22:31:29 +0530

    2 files added

--- src/Component/NewsBoard.jsx
+++ src/Component/NewsBoard.jsx
@@ -4,12 +4,11 @@ import NewsItem from './NewsItem';
 
 const NewsBoard = ({category}) => {
   const [articles, setArticles] = useState([]);
-  const apiKey = "eb792f7564244e7db15d01b7545bc439";
+  const apiKey = "YOUR API KEY ADD HERE";
   
 
   useEffect(() => {
     let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${apiKey}`
-    // let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=eb792f7564244e7db15d01b7545bc439`;
     // let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
     
     fetch(url)
